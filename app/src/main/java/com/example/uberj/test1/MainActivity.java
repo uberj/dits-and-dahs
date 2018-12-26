2aeb11806563aa59d5eb26f2913edb56f64aa70d
Jacques Uber <uberj@miradorfin.com>
2018-12-25 20:55:49 -0600

    delete fragment. don't know what that is

--- app/src/main/java/com/example/uberj/test1/MainActivity.java
+++ app/src/main/java/com/example/uberj/test1/MainActivity.java
@@ -3,10 +3,7 @@ package com.example.uberj.test1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-
-import com.example.uberj.test1.ui.mainscreen.MainScreenFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,12 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_screen_activity);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, MainScreenFragment.newInstance())
-                    .commitNow();
-        }
-
         Button buttonTraining = findViewById(R.id.btnTraining);
         buttonTraining.setOnClickListener((view) -> {
             this.startActivity(new Intent(this, TrainingActivityList.class));
--- app/src/main/java/com/example/uberj/test1/ui/mainscreen/MainScreenViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.uberj.test1.ui.mainscreen;
-
-import android.arch.lifecycle.ViewModel;
-
-public class MainScreenViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
