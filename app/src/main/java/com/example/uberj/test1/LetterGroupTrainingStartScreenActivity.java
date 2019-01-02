0e4eedaaced2f433dc48bb349ff64e9865c0c7bb
Jacques Uber <uberj@miradorfin.com>
2019-01-02 08:20:06 -0800

    wip

--- app/src/main/java/com/example/uberj/test1/LetterGroupTrainingStartScreenActivity.java
+++ app/src/main/java/com/example/uberj/test1/LetterGroupTrainingStartScreenActivity.java
@@ -4,11 +4,12 @@ import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class KeyboardActivity extends AppCompatActivity {
+public class LetterGroupTrainingStartScreenActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_letter_group_training_start_screen);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        setContentView(R.layout.keyboard_activity);
     }
 }
