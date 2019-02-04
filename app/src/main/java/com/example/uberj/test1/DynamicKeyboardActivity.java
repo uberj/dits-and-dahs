c9540b86d6f5bafbd1c0f514405408446897b56c
Jacques Uber <uberj@miradorfin.com>
2019-02-04 08:55:18 -0800

    move to declarative weights in the keyboard definition

--- app/src/main/java/com/example/uberj/test1/DynamicKeyboardActivity.java
+++ app/src/main/java/com/example/uberj/test1/DynamicKeyboardActivity.java
@@ -11,38 +11,47 @@ import android.widget.TextView;
 
 import com.google.common.collect.ImmutableList;
 
+import org.apache.commons.lang3.tuple.Pair;
+
 /*
 This class is for exploring dynamicly creating a keyboard at runtime using the java layout apis
  */
 public class DynamicKeyboardActivity extends Activity {
-    private static final String HSM = "HALF_SPACE_MARKER";
-    ImmutableList<ImmutableList<String>> keys = ImmutableList.of(
-                           ImmutableList.of("1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "/", "="),
-                      ImmutableList.of(HSM, "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", ".", HSM),
-                 ImmutableList.of(HSM, HSM, "a", "s", "d", "f", "g", "h", "j", "k", "l", "?", HSM, HSM),
-            ImmutableList.of(HSM, HSM, HSM, "z", "x", "c", "v", "b", "n", "m", ",", ".", HSM, HSM, HSM)
+    private static final Pair<Float, String> HSM = Pair.of(0.5f, null);
+    ImmutableList<ImmutableList<Pair<Float, String>>> keys = ImmutableList.of(
+            ImmutableList.of(s1("1"), s1("2"), s1("3"), s1("4"), s1("5"), s1("6"), s1("7"), s1("8"), s1("9"), s1("0"), s1("/"), s1("=")),
+            ImmutableList.of(HSM, s1("q"), s1("w"), s1("e"), s1("r"), s1("t"), s1("y"), s1("u"), s1("i"), s1("o"), s1("p"), s1("."), HSM),
+            ImmutableList.of(HSM, HSM, s1("a"), s1("s"), s1("d"), s1("f"), s1("g"), s1("h"), s1("j"), s1("k"), s1("l"), s1("?"), HSM, HSM),
+            ImmutableList.of(HSM, HSM, HSM, s1("z"), s1("x"), s1("c"), s1("v"), s1("b"), s1("n"), s1("m"), s1(","), s1("."), HSM, HSM, HSM)
     );
+
+    private Pair<Float, String> s1(String s) {
+        return Pair.of(1f, s);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.keyboard_activity_v2);
         LinearLayout rootView = findViewById(R.id.dynamic_keyboard);
-        for (ImmutableList<String> row : keys) {
+        for (ImmutableList<Pair<Float, String>> row : keys) {
             LinearLayout curRow = new LinearLayout(this);
             curRow.setOrientation(LinearLayout.HORIZONTAL);
             float weightSum = calcWeightSumForRow(row);
             curRow.setWeightSum(weightSum);
-            for (String keyName : row) {
-                if (keyName.equals(HSM)) {
+            for (Pair<Float, String> cell : row) {
+                Float weight = cell.getLeft();
+                String keyName = cell.getRight();
+                if (keyName == null) {
                     Space space = new Space(this);
                     LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                            0, ViewGroup.LayoutParams.WRAP_CONTENT, 0.5f);
+                            0, ViewGroup.LayoutParams.WRAP_CONTENT, weight);
                     space.setLayoutParams(params);
                     curRow.addView(space);
                 } else {
                     LinearLayout buttonProgressBarContainer = new LinearLayout(this);
                     LinearLayout.LayoutParams buttonProgressBarContainerParams = new LinearLayout.LayoutParams(
-                            0, ViewGroup.LayoutParams.WRAP_CONTENT, 1f);
+                            0, ViewGroup.LayoutParams.WRAP_CONTENT, weight);
                     buttonProgressBarContainer.setLayoutParams(buttonProgressBarContainerParams);
                     buttonProgressBarContainer.setOrientation(LinearLayout.VERTICAL);
 
@@ -58,16 +67,11 @@ public class DynamicKeyboardActivity extends Activity {
         }
     }
 
-    private float calcWeightSumForRow(ImmutableList<String> row) {
+    private float calcWeightSumForRow(ImmutableList<Pair<Float, String>> row) {
         float total = 0;
-        for (String s : row) {
-            if (s.equals(HSM)) {
-                total += 0.5;
-            } else {
-                total += 1;
-            }
+        for (Pair<Float, String> pair : row) {
+            total += pair.getLeft();
         }
-
         return total;
     }
 
