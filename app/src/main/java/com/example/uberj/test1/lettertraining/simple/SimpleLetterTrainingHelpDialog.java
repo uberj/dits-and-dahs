9b86a825aefc1a33e3077f22a9aef305e7969f63
Jacques Uber <uberj@miradorfin.com>
2019-02-22 09:26:44 -0800

    use tabs instead of trying to get all help onto one page. going to different tabs to explain different things

--- app/src/main/java/com/example/uberj/test1/lettertraining/simple/SimpleLetterTrainingHelpDialog.java
+++ app/src/main/java/com/example/uberj/test1/lettertraining/simple/SimpleLetterTrainingHelpDialog.java
@@ -1,5 +1,6 @@
 package com.example.uberj.test1.lettertraining.simple;
 
+
 import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,11 +13,17 @@ import com.example.uberj.test1.R;
 import com.example.uberj.test1.keyboards.KeyConfig;
 import com.google.common.collect.ImmutableList;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 public class SimpleLetterTrainingHelpDialog extends DialogFragment {
     @NonNull
@@ -39,17 +46,85 @@ public class SimpleLetterTrainingHelpDialog extends DialogFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        View inflate = inflater.inflate(R.layout.simple_letter_training_help_dialog, container, false);
-        DynamicKeyboard builder = new DynamicKeyboard.Builder()
-                .setContext(getActivity())
-                .setKeys(ImmutableList.of(ImmutableList.of(KeyConfig.l("M"))))
-                .setButtonCallback((b) -> {})
-                .setProgressBarCallback((p, v) -> {})
-                .createKeyboardBuilder();
 
-        LinearLayout exampleLetterKeyContainer = inflate.findViewById(R.id.example_letter_key);
-        builder.buildAtRoot(exampleLetterKeyContainer);
+        View view = inflater.inflate(R.layout.simple_letter_training_help_dialog, container, false);
+        // Setting ViewPager for each Tabs
+        ViewPager viewPager = view.findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+        // Set Tabs inside Toolbar
 
-        return inflate;
+        return view;
+    }
+
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public Adapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        Adapter adapter = new Adapter(getChildFragmentManager());
+        adapter.addFragment(new HelpScreen1(), "HelpScreen1");
+        adapter.addFragment(new HelpScreen2(), "HelpScreen2");
+        viewPager.setAdapter(adapter);
+    }
+
+    public static class HelpScreen1 extends Fragment {
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            super.onCreateView(inflater, container, savedInstanceState);
+            View inflate = inflater.inflate(R.layout.simple_letter_training_help_dialog_screen1, container, false);
+            DynamicKeyboard builder = new DynamicKeyboard.Builder()
+                    .setContext(getActivity())
+                    .setKeys(ImmutableList.of(ImmutableList.of(KeyConfig.l("M"))))
+                    .setButtonCallback((b) -> {})
+                    .setProgressBarCallback((p, v) -> {})
+                    .createKeyboardBuilder();
+
+            LinearLayout exampleLetterKeyContainer = inflate.findViewById(R.id.example_letter_key);
+            builder.buildAtRoot(exampleLetterKeyContainer);
+            return inflate;
+        }
+    }
+
+    public static class HelpScreen2 extends Fragment {
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            super.onCreateView(inflater, container, savedInstanceState);
+            View inflate = inflater.inflate(R.layout.simple_letter_training_help_dialog_screen1, container, false);
+            DynamicKeyboard builder = new DynamicKeyboard.Builder()
+                    .setContext(getActivity())
+                    .setKeys(ImmutableList.of(ImmutableList.of(KeyConfig.l("N"))))
+                    .setButtonCallback((b) -> {})
+                    .setProgressBarCallback((p, v) -> {})
+                    .createKeyboardBuilder();
+
+            LinearLayout exampleLetterKeyContainer = inflate.findViewById(R.id.example_letter_key);
+            builder.buildAtRoot(exampleLetterKeyContainer);
+            return inflate;
+        }
     }
 }
